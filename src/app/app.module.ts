5a3c96610c35675cfba5851cd197cb4282df4c82
BarbaraLO <barbaraleivao@gmail.com>
2022-10-01 18:35:02 -0300

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { FormularioUsuarioComponent } from './formulario-usuario/formulario-usuario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    InicioSesionComponent,
+    FormularioUsuarioComponent
   ],
   imports: [
     BrowserModule,

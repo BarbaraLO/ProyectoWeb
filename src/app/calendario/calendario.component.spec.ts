9cdae8225156531506698dbc4f5e1715fbb50973
BarbaraLO <barbaraleivao@gmail.com>
2022-10-02 03:35:02 -0300

    modificaciones para calendario

--- src/app/calendario/calendario.component.spec.ts
+++ src/app/calendario/calendario.component.spec.ts
@@ -3,9 +3,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CalendarioComponent } from './calendario.component';
 
 describe('CalendarioComponent', () => {
-  let component: CalendarioComponent;
+  let component: FormularioUsuarioComponent;
   let fixture: ComponentFixture<CalendarioComponent>;
 
+  (document).ready(function() {
+    ("#basic-form").validate();
+  });
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ CalendarioComponent ]

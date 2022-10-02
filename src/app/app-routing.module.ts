79aa2842fc41789532849e765af09e9ada3fdfe6
NaomieC99 <naomie.contreras.q@mail.pucv.cl>
2022-10-02 00:55:54 -0300

    import formulario-usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { InicioSesionComponent } from './inicio-sesion/inicio-sesion.component';
+import { FormularioUsuarioComponent } from './formulario-usuario/formulario-usuario.component';
 
 const routes: Routes = [ 
   { path: 'inicio', component: InicioComponent },
-  { path: 'inicio-sesion', component: InicioSesionComponent}
+  { path: 'inicio-sesion', component: InicioSesionComponent},
   { path: 'formulario-usuario', component: FormularioUsuarioComponent},
 ];
 

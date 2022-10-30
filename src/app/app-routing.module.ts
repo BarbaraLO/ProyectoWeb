9695d02f9e1a2df741f57ce039227f0e28c981ea
NaomieC99 <naomie.contreras.q@mail.pucv.cl>
2022-10-30 00:16:15 -0300

    formulario de inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
-import { InicioSesionComponent } from 'ProyectoWeb/src/app/inicio-sesion/inicio-sesion.component';
+import { InicioSesionService } from 'ProyectoWeb/src/app/inicio-sesion.service';
 import { FormularioUsuarioComponent } from './formulario-usuario/formulario-usuario.component';
 import { FuncionesComponent } from './funciones/funciones.component';
 import { EspecialidadesComponent } from './especialidades/especialidades.component';
@@ -9,19 +9,21 @@ import { CalendarioComponent } from './calendario/calendario.component';
 import { ProfesionalesComponent } from './profesionales/profesionales.component';
 import { CitaComponent } from './cita/cita.component';
 import { CitasPendientesComponent } from './citas-pendientes/citas-pendientes.component';
+import { FormularioInicioComponent } from './formulario-inicio/formulario-inicio.component';
 
 
 const routes: Routes = [ 
   { path: '', component: InicioComponent },
   { path: 'inicio', component: InicioComponent },
-  { path: 'inicio-sesion', component: InicioSesionComponent},
+  { path: 'inicio-sesion', component: InicioSesionService},
   { path: 'formulario-usuario', component: FormularioUsuarioComponent},
   { path: 'funciones', component: FuncionesComponent},
   { path: 'especialidades', component: EspecialidadesComponent},
   { path: 'calendario', component: CalendarioComponent},
   { path: 'profesionales', component: ProfesionalesComponent},
   { path: 'cita', component: CitaComponent},
-  { path: 'citas-pendientes', component: CitasPendientesComponent}
+  { path: 'citas-pendientes', component: CitasPendientesComponent},
+  { path: 'formulario-inicio', component: FormularioInicioComponent}
 
 ];
 
